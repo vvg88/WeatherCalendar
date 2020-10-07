18590a35fd4f331442b0861c374994e432585722
Vladimir Grishanin <grishaninv.v@gmail.com>
2020-10-07 20:07:25 +0300

    Refactor getting weather page

--- main.go
+++ main.go
@@ -7,22 +7,37 @@ import (
 )
 
 func main() {
-	getFromYandex()
+	readYaPage()
 }
 
-func getFromYandex() {
-	resp, err := http.Get("https://yandex.ru/pogoda/?lat=56.979999&lon=40.983071")
+func readYaPage() {
+	page, err := readHttpPage("https://yandex.ru/pogoda/?lat=56.979999&lon=40.983071")
+	err = savePage("yaPage.txt", page)
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func readHttpPage(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
+		return nil, err
 	}
-	err = ioutil.WriteFile("yaPage.txt", body, 0600)
+	return body, err
+}
+
+func savePage(file string, page []byte) error {
+	err := ioutil.WriteFile(file, page, 0600)
 	if err != nil {
 		log.Fatal(err)
 	}
+	return err
 }
