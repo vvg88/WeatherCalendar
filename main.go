0f2d3bf1a66e5d88990207269e54bbe58156c371
Vladimir Grishanin <grishaninv.v@gmail.com>
2020-10-12 21:47:48 +0300

    Parse all required values

--- main.go
+++ main.go
@@ -12,14 +12,14 @@ func main() {
 }
 
 func readYaPage() {
-	page, err := readHttpPage("https://yandex.ru/pogoda/?lat=56.979999&lon=40.983071")
+	page, err := readHTTPPage("https://yandex.ru/pogoda/?lat=56.979999&lon=40.983071")
 	err = savePage("yaPage.txt", page)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func readHttpPage(url string) ([]byte, error) {
+func readHTTPPage(url string) ([]byte, error) {
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
--- parser.go
+++ parser.go
@@ -7,8 +7,17 @@ import (
 	"regexp"
 )
 
-var tempRegx = regexp.MustCompile(`Текущая температура<\/span><span class="temp__value">([+-]\d+)<\/span>`)
-var windSpeedRegx = regexp.MustCompile(`wind-speed">(\d{1,2}(,\d)?)<\/span>`)
+const tmpExpr = `Текущая температура<\/span><span class="temp__value">([+-]\d+)<\/span>`
+const windSpeedExpr = `wind-speed">(\d{1,2}(,\d)?)<\/span>`
+const wethCondExpr = `link__condition day-anchor i-bem" data-bem='{"day-anchor":{"anchor":\d+}}'>([а-яА-Я\s]+)`
+const humExpr = `icon_humidity-white term__fact-icon" aria-hidden="true"><\/i>(\d{1,2})`
+const pressExpr = `icon_pressure-white term__fact-icon" aria-hidden="true"><\/i>(?P<press>\d{3})`
+
+var tempRegx = regexp.MustCompile(tmpExpr)
+var windSpeedRegx = regexp.MustCompile(windSpeedExpr)
+var conditionRegx = regexp.MustCompile(wethCondExpr)
+var humidRegx = regexp.MustCompile(humExpr)
+var pressRegx = regexp.MustCompile(pressExpr)
 
 func parsePage(name string) {
 	page, err := ioutil.ReadFile(name)
@@ -18,15 +27,35 @@ func parsePage(name string) {
 
 	submatches := tempRegx.FindSubmatch(page)
 	if submatches == nil {
-		log.Fatal("No temperature matches found!")
+		fmt.Println("No temperature matches found!")
 	}
 	fmt.Printf("A current temperature is %s\n", string(submatches[1]))
 
 	submatches = windSpeedRegx.FindSubmatch(page)
 	if submatches == nil {
-		log.Fatal("No wind speed matches found!")
+		fmt.Println("No wind speed matches found!")
 	}
-	fmt.Printf("A wind speed is %s", string(submatches[1]))
+	fmt.Printf("A wind speed is %s\n", string(submatches[1]))
+
+	submatches = conditionRegx.FindSubmatch(page)
+	if submatches == nil {
+		fmt.Println("No weather condition found!")
+	} else {
+		fmt.Printf("Weather condition: %s\n", string(submatches[1]))
+	}
+
+	submatches = humidRegx.FindSubmatch(page)
+	if submatches == nil {
+		fmt.Println("No humidity found!")
+	}
+	fmt.Printf("Humidity: %s", string(submatches[1]))
+	fmt.Println("%")
+
+	submatches = pressRegx.FindSubmatch(page)
+	if submatches == nil {
+		fmt.Println("No air pressure found!")
+	}
+	fmt.Printf("Air pressure: %s", string(submatches[1]))
 }
 
 func readPage(name string) (string, error) {
