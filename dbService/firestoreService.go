ddc16b649001544d91a10e9939826440e16e7b8e
Vladimir Grishanin <grishaninv.v@gmail.com>
2021-02-13 21:00:40 +0300

    Containerize firestore service

--- dbService/firestoreService.go
+++ dbService/firestoreService.go
@@ -20,13 +20,13 @@ func main() {
 	ctx = context.Background()
 	var err error
 	fsClient, err = createClient(ctx)
+	defer fsClient.Close()
 	if err != nil {
 		log.Fatalf("Unable to create firestore client!\nError: %v", err)
 	}
 
 	r.HandleFunc("/dbservice/weatherdata", postWeatherData).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8080", r))
-
 }
 
 func postWeatherData(w http.ResponseWriter, r *http.Request) {
@@ -34,16 +34,20 @@ func postWeatherData(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Printf("Error reading POST request body: %v", err)
 	}
+	log.Println(string(reqBody))
 
 	var wd weatherData
 	err = json.Unmarshal(reqBody, &wd)
 	if err != nil {
 		log.Printf("Error parsing POST json: %v", err)
 	}
+	log.Println(wd)
 
 	fswd := wd.toFirestoreData()
+	log.Println(fswd)
 	err = fswd.save()
 	if err != nil {
 		log.Printf("Error saving weather data to FireStore: %v", err)
 	}
+	log.Println("Value saved!")
 }
--- dbService/go.mod
+++ dbService/go.mod
@@ -1,3 +1,8 @@
 module github.com/vvg88/WeatherCalendar/dbService
 
 go 1.15
+
+require (
+	cloud.google.com/go/firestore v1.4.0
+	github.com/gorilla/mux v1.8.0
+)
