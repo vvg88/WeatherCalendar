afd3e68b2fa353a136e607bf7b1b901bbd71f99b
Vladimir Grishanin <grishaninv.v@gmail.com>
2022-01-04 12:25:38 +0300

    Script to backup data

---
+++ utility/backupData.ps1
@@ -0,0 +1,2 @@
+$env:GOOGLE_APPLICATION_CREDENTIALS="C:\Users\vladi\go\src\github.com\vvg\WeatherCalendar\dbService\gcloudFsKey\weather-calend-fires-2a048fef69e5.json"
+& 'C:\Users\vladi\go\src\github.com\vvg\WeatherCalendar\utility\utility.exe' -mode backupData
